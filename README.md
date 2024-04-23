0baff2ca0e4a3de4d9ab7e5ab8d31d6e6d1eb785
tien.nguyenduc <tien.nguyenduc@hust.edu.vn>
2024-04-24 00:38:27 +0700

    demo oled 1.3"

---
+++ README.md
@@ -0,0 +1,17 @@
+# GAMEPAD
+
+Máy chơi game
+
+## Tính năng
+
+1. Các trò chơi dựa trên 4 nút bấm
+
+## Danh sách linh kiện
+
+|Modules|Quantity| Connection Pins|
+|--|--|--|
+|ESP32S2 D1 Mini|1| |
+|Nút bấm 3x6x2.5  SMD|4| Up = GPIO12, Left = GPIO14, Down = GPIO9; Right = GPIO11|
+|Nút bấm phẳng 4x4x0.8 SMD |4| GPIO7|
+|Nút chạm Touch|2| GPIO3, GPIO5|
+|Oled 0.96" I2C|1| SCL=39, SDA=37|
---
+++ src/main.ino
@@ -0,0 +1,88 @@
+#define OLED_SCK_PIN 39
+#define OLED_SDA_PIN 37
+#define LED_BUILDIN 15
+#include <Arduino.h>
+
+/*
+
+  Terminal.ino
+  
+  Demo for U8log
+  
+  Universal 8bit Graphics Library (https://github.com/olikraus/u8g2/)
+
+  Copyright (c) 2016, olikraus@gmail.com
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without modification, 
+  are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice, this list 
+    of conditions and the following disclaimer.
+    
+  * Redistributions in binary form must reproduce the above copyright notice, this 
+    list of conditions and the following disclaimer in the documentation and/or other 
+    materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
+
+*/
+
+#include <Arduino.h>
+#include <U8x8lib.h>
+
+U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE, OLED_SCK_PIN, OLED_SDA_PIN);
+
+#define U8LOG_WIDTH 16
+#define U8LOG_HEIGHT 8
+uint8_t u8log_buffer[U8LOG_WIDTH*U8LOG_HEIGHT];
+U8X8LOG u8x8log;
+
+
+
+void setup(void)
+{
+  /* U8g2 Project: SSD1306 Test Board */
+  //pinMode(10, OUTPUT);
+  //pinMode(9, OUTPUT);
+  //digitalWrite(10, 0);
+  //digitalWrite(9, 0);		
+  
+  /* U8g2 Project: KS0108 Test Board */
+  //pinMode(16, OUTPUT);
+  //digitalWrite(16, 0);	
+  
+  u8x8.begin();
+  u8x8.setFont(u8x8_font_chroma48medium8_r);
+  
+  u8x8log.begin(u8x8, U8LOG_WIDTH, U8LOG_HEIGHT, u8log_buffer);
+  u8x8log.setRedrawMode(1);		// 0: Update screen with newline, 1: Update screen for every char  
+  
+}
+
+unsigned long t = 0;
+
+// print the output of millis() to the terminal every second
+void loop(void) {
+  if ( t < millis() ) {
+    t = millis() + 15000;			// every 15 seconds
+    u8x8log.print("\f");			// \f = form feed: clear the screen
+  }
+  u8x8log.print("millis=");
+  u8x8log.print(millis());
+  u8x8log.print("\n");
+  delay(500);
+}
+
